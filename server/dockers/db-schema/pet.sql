e0536e7e8f8b5f0cf8a82865212df0e6cab5cd36
Arthur Layese <arthur@layese.com>
2022-05-17 14:31:11 +0800

    integrated JWT

--- server/dockers/db-schema/pet.sql
+++ server/dockers/db-schema/pet.sql
@@ -8,3 +8,12 @@ CREATE TABLE dogs(
     PRIMARY KEY (id),
     INDEX (name)
 );
+
+CREATE TABLE users(
+    id INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(200) NOT NULL,
+    password VARCHAR(200) NOT NULL,
+    PRIMARY KEY (id),
+    INDEX (username)
+);
+INSERT INTO users (username, password) VALUES ('admin', 'admin');

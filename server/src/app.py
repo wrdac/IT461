8ad181c4177357063680427588692e5713c96949
Arthur Layese <arthur@layese.com>
2022-05-24 16:57:56 +0800

    updates on api

--- server/src/app.py
+++ server/src/app.py
@@ -1,9 +1,18 @@
+import json
 from flask import Flask, request, jsonify, g
 from v1.dog.router import DogRouter
 from v1.auth import login as auth_login, verify_token as auth_verify_token
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'I/L0ve/CIT-U'
+app.config['CORS_HEADERS'] = ['Content-Type', 'authorization']
+
+#CORS(app)
+CORS(app, resources={r"*": {"origins": [
+    'http://127.0.0.1:3000',
+    'http://localhost:3000',
+]}},  supports_credentials=True)
 
 app.register_blueprint(DogRouter.handler())
 
@@ -16,6 +25,10 @@ def login():
             return jsonify({'token': token})
     return jsonify({'message': 'Invalid username or password'}), 403
 
+@app.route('/v1/login')
+def home():
+    return jsonify({'message': 'hello world'})
+
 @app.route('/v1/verify-token')
 def verify_token():
     token = request.args.get('token')
@@ -24,4 +37,4 @@ def verify_token():
     return jsonify({'ok': 'Token is valid'})
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0',port=6000)
+    app.run(debug=True,host='0.0.0.0',port=8080)
